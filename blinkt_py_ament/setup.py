74f4c8a166c0acb5fb87305501f7a46cd03978cb
James Pace <jpace121@gmail.com>
2021-04-20 20:35:25 +0000

    Try to use sym links.

--- blinkt_py_ament/setup.py
+++ blinkt_py_ament/setup.py
@@ -61,6 +61,11 @@ class CMakeBuild(build_ext):
 setup(
     name=package_name,
     version='0.0.0',
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=False,
     maintainer='James Pace',
